77904d50eb256aa3147a2311d93efa8d6f4cfea8
sam hinton <sam.hinton88@gmail.com>
2018-06-27 09:53:36 +0100

    redeploy

---
+++ src/apidemy/components/InternationalExperienceShop/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class InternationalExperienceShop extends Component {
+
+  render() {
+
+    return(
+      <div className='international-experience-shop'>
+
+      </div>
+    )
+  }
+}
+
+export default InternationalExperienceShop;
